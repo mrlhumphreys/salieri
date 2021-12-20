016d355bd046f0c5ff1010565650ebe7c7b6f245
Mark Humphreys <mark@mrlhumphreys.com>
2021-12-21 10:25:18 +1100

    duplicate dice if double rolled

--- src/backgammon/state/dice_set.rs
+++ src/backgammon/state/dice_set.rs
@@ -5,11 +5,28 @@ pub fn parse_dice_set(encoded: &str) -> Result<Vec<Die>, &'static str> {
     if encoded.len() == 2 {
         let mut dice = Vec::new();
     
-        for c in encoded.chars() {
-            match parse_die(c) {
-                Ok(d) => dice.push(d),
-                Err(e) => return Err(e)
+
+        if &encoded[0..1] == &encoded[1..2] {
+            let characters = encoded.chars();
+            for c in characters {
+                match parse_die(c) {
+                    Ok(d) => {
+                        // double rolled, duplicate dice
+                        dice.push(d.clone());
+                        dice.push(d);
+                    },
+                    Err(e) => return Err(e)
+                }
             }
+        } else {
+            let characters = encoded.chars();
+            for c in characters {
+                match parse_die(c) {
+                    Ok(d) => dice.push(d),
+                    Err(e) => return Err(e)
+                }
+            }
+
         }
 
         Ok(dice)
@@ -29,6 +46,15 @@ mod tests {
        assert_eq!(dice.len(), 2);
        let die = &dice[0];
        assert_eq!(die.number.unwrap(), 1);
+    }
+
+    #[test]
+    fn parsing_double_dice_set_test() {
+       let encoded = "22"; 
+       let dice = parse_dice_set(encoded).unwrap();
+       assert_eq!(dice.len(), 4);
+       let die = &dice[0];
+       assert_eq!(die.number.unwrap(), 2);
     }
 
     #[test]
