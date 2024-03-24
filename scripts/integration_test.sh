a195452a79b3c8acf46d45d0ab1c51d4932e3423
Mark Humphreys <mark@mrlhumphreys.com>
2024-03-24 11:38:11 +1000

    checkers - fix integration tests

--- scripts/integration_test.sh
+++ scripts/integration_test.sh
@@ -32,13 +32,13 @@ assert_post_response() {
 }
 
 # checkers tests
-assert_post_response "checkers move" "http://localhost:7878/api/v0/checkers" "bbbbbbbbb-bb--b-----wwwwwwwwwwwww" "24-20"
+assert_post_response "checkers move" "http://localhost:7878/api/v0/checkers" "W:W16,19,20,21,22,27,28,29,30,31,32:B1,2,3,4,5,6,7,9,11,12" "31-26"
 
-assert_post_response "checkers openings db" "http://localhost:7878/api/v0/checkers/openings_db" "bbbbbbb-bbbb--b---w-ww-wwwwwwwwww" "22-17"
+assert_post_response "checkers openings db" "http://localhost:7878/api/v0/checkers/openings_db" "B:W18,21,22,24,25,26,27,28,29,30,31,32:B1,2,3,4,5,6,7,8,10,11,12,13" "11-16"
 
-assert_post_response "checkers minimax" "http://localhost:7878/api/v0/checkers/minimax" "bbbbbbb-bbbb--b---w-ww-wwwwwwwwww" "21-17"
+assert_post_response "checkers minimax" "http://localhost:7878/api/v0/checkers/minimax" "W:W16,19,20,21,22,27,28,29,30,31,32:B1,2,3,4,5,6,7,9,11,12" "21-17"
 
-assert_post_response "checkers mcts" "http://localhost:7878/api/v0/checkers/mcts" "bbbbbbb-bbbb--b---w-ww-wwwwwwwwww" "27-23"
+assert_post_response "checkers mcts" "http://localhost:7878/api/v0/checkers/mcts" "W:W16,19,20,21,22,27,28,29,30,31,32:B1,2,3,4,5,6,7,9,11,12" "31-26"
 
 # backgammon tests
 assert_post_response "backgammon move" "http://localhost:7878/api/v0/backgammon" "0020000000000500300000005005000000030050000000000200121" "2-1: 19/21 21/22"
@@ -50,6 +50,8 @@ assert_post_response "backgammon minimax" "http://localhost:7878/api/v0/backgamm
 assert_post_response "backgammon mcts" "http://localhost:7878/api/v0/backgammon/mcts" "0020000000000500300000005005000000030050000000000200121" "2-1: 19/21 21/22"
 
 # chess tests
-assert_post_response "chess move" "http://localhost:7878/api/v0/chess" "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1" "d4"
+assert_post_response "chess move" "http://localhost:7878/api/v0/chess" "rnbqkb1r/pp3ppp/3p1n2/2pP4/8/2N5/PP2PPPP/R1BQKBNR w KQkq - 0" "Qb3"
 
-assert_post_response "chess minimax" "http://localhost:7878/api/v0/chess/minimax" "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1" "d4"
+assert_post_response "chess openings db" "http://localhost:7878/api/v0/chess/openings_db" "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1" "e4"
+
+assert_post_response "chess minimax" "http://localhost:7878/api/v0/chess/minimax" "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1" "e4"
